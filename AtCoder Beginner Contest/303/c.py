ef014951e9ac3456f6f42854485d9ca246a97cc0
masayasekine <negirider250@gmail.com>
2023-06-09 17:13:59 +0900

    303

--- AtCoder Beginner Contest/303/c.py
+++ AtCoder Beginner Contest/303/c.py
@@ -1,17 +1,7 @@
 def main():
-    # setを使いましょう
     N, M, H, K = map(int, input().split())
     S = input()
-    X = []
-    Y = []
-    # for _ in range(M):
-    #     A, B = map(int, input().split())
-    #     X.append(A)
-    #     Y.append(B)
-    A = []
-    for _ in range(M):
-        A.append(list(map(int, input().split())))
-    B = set(A)
+    P = set([tuple(map(int,input().split())) for i in range(M)])
     
 
     x = 0
@@ -30,19 +20,9 @@ def main():
             print('No')
             return
         if H < K:
-            if x not in X or y not in Y:
-                continue
-            tmp = [x,y]
-            if tmp in B:
+            if (x,y) in P:
                 H = K
-                A.remove(tmp)
-            # for i, a in enumerate(X):
-            #     if x == a:
-            #         if y == Y[i]:
-            #             H = K
-            #             del X[i]
-            #             del Y[i]
-            #             break
+                P.remove((x,y))
 
     print('Yes')
 main()
